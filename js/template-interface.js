025de75b0613425c7ffd0c4419960abe538d17f1
Valia Dempsey <valentinaml@gmail.com>
2017-12-01 15:14:50 -0800

    Build initial files.

--- js/template-interface.js
+++ js/template-interface.js
@@ -1,1 +1,27 @@
-import { ClassName } from './../js/template-backend.js';
+var apiKey = require('./../.env').apiKey;
+import { Doctor } from './../js/template-backend.js';
+
+
+let displayData = function(results) {
+  for (let i=0; i<10; i++) {
+    let docFullName = results.data.profile.first_name;
+    console.log(docFullName);
+    // let name = data.profile.first_name;
+     $('.output').append("<li>" + docFullName[i] + "</li>");
+ }
+};
+
+$(document).ready(function() {
+
+  $("form#find").submit(function(event) {
+    let condition = $("#condition").val();
+    event.preventDefault();
+
+    let newSearch = new Doctor(condition);
+    let apiResults = newSearch.apiConstructor(displayData);
+  });
+
+  $(".btn-error").click(function(event) {
+    $('.output').empty();
+  });
+});
